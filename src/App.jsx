0796821df103e502469b466517317f98d462e367
randaegs <garciasanchezrd@gmail.com>
2024-05-05 22:09:39 -0400

    Header and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import './App.css'
-
 function App() {
 
+  return (
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col">
+          <h1 className="text-center">Todo App</h1>
+          <div className="text-center">
+            <input type="text" placeholder="Type task" id="create"></input>
+            <button className="btn btn-primary">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
