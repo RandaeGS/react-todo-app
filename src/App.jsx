380b977309ee24f86270e62c0a136e7e94835335
randaegs <garciasanchezrd@gmail.com>
2024-05-06 17:01:22 -0400

    Creating buttons to switch between done and undone tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css"
+import TodoList from "./TodoList"
 
 function App() {
+
+  const handleActiveClick = () => {
+
+  }
+
+  const handleDoneClick = () => {
+
+  }
 
   return (
     <div className="container text-center" id="container">
       <div className="row align-items-center">
         <div className="col">
           <h1 className="text-center">Todo App</h1>
+          <button type="button" className="btn btn-primary mx-2" onClick={handleActiveClick}>Active</button>
+          <button type="button" className="btn btn-secondary" onClick={handleDoneClick}>Done</button>
+
+          <TodoList />
         </div>
       </div>
     </div>
